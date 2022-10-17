a0ac141824ad635cdba650e6939b52029f66d4f2
unclesmoove <alex.f.ushe@gmail.com>
2022-10-17 16:24:38 +0200

    recipe details, shopping-list, recipes components added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] =[
+  new Recipe('A test recipe','this is my first recipe',
+  'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/04/27/FN_FNK_Air-Fryer-Steak-With-Garlic-Herb-Butter_s4x3.jpg.rend.hgtvcom.966.725.suffix/1588019651595.jpeg'),
+  new Recipe('A test recipe','this is my first recipe',
+  'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/04/27/FN_FNK_Air-Fryer-Steak-With-Garlic-Herb-Butter_s4x3.jpg.rend.hgtvcom.966.725.suffix/1588019651595.jpeg')
+];
+
 
   constructor() { }
 
